9e8f2001b917e0ae073eeb4b73a80b722818c4fa
mariogmarq <mariogarciamarq@gmail.com>
2021-04-25 10:53:13 +0200

    BP2 terminado :)

--- bp2/ejer11/pmv-OpenMP-script.sh
+++ bp2/ejer11/pmv-OpenMP-script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
+#SBATCH --ntasks=1
 
 # Script para atcgrid
 
-echo "atcgrid1-3"
-vec=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,32)
+vec=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 32)
 for i in "${vec[@]}"; do
-		echo $i
-		srun -c$i -n1 -pac ./a.out 7000
-		srun -c$i -n1 -pac ./a.out 50000
-done
-echo "atcgrid4"
-for i in "${vec[@]}"; do
-		echo $i
-		srun -c$i -n1 -p ac4 ./a.out 7000
-		srun -c$i -n1 -p ac4 ./a.out 50000
+		export OMP_NUM_THREADS=$i
+		echo "$i 7k"
+		./a.out 7000
+		echo "$i 50k"
+		./a.out 50000
 done
